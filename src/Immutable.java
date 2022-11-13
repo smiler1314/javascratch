57eec93d033b77470e4655d7e1c669a107f2da9f
Damian Charles Lowe <damian_c_lowe@yahoo.co.uk>
2022-11-13 00:47:02 +0000

    add Immutable Data Container enabling FP style

---
+++ src/Immutable.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Immutable {
+
+    public static void main(String[] args) {
+        final Container<String> data = new Container<>(Arrays.asList("h", "2", "2"));
+        System.out.println(data);
+        final Container<String> data2 = data.addThing("thing1");
+        System.out.println(data2);
+        data2.getStreamOfThings()
+                .forEach(System.out::println);
+        data2.getStreamOfThings()
+                .map(t -> "ttt..." + t + "...")
+                .forEach(System.out::println);
+
+        System.out.println("-----------");
+
+        new Container<String>()
+                .addThing("1")
+                .addThing("r2")
+                .addThing("3")
+                .addThing("3")
+                .addThings(Arrays.asList("5", "7", "9"))
+                .getStreamOfThings()
+//                .peek(System.out::print)
+                .mapToInt(thing -> safeParseInt(thing).orElse(0))
+                .flatMap(i -> IntStream.of(i, i + 1))
+                .map(i -> 2 * i)
+                .forEach(System.out::println);
+    }
+
+    public static Optional<Integer> safeParseInt(Object in) {
+        try {
+            return Optional.of(Integer.parseInt(in.toString()));
+        } catch (NumberFormatException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    public static class Container<T> {
+        private final Set<T> things;
+
+        Container(Collection<T> things) {
+            this.things = new LinkedHashSet<>(things);
+        }
+
+        Container() {
+            this(Collections.emptySet());
+        }
+
+        public Stream<T> getStreamOfThings() {
+            return things.stream();
+        }
+
+        public Container<T> addThing(T thing) {
+            return new Container<>(Stream.concat(
+                    things.stream(),
+                    Stream.of(thing)
+            ).collect(Collectors.toUnmodifiableSet())
+            );
+        }
+
+        public Container<T> addThings(Collection<T> otherThings) {
+            return new Container<>(Stream.concat(
+                    things.stream(),
+                    otherThings.stream()
+            ).collect(Collectors.toUnmodifiableSet())
+            );
+        }
+
+        @Override
+        public String toString() {
+            return things.toString();
+        }
+    }
+}
