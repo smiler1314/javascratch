dbca3dbbc73bc3ec54136b99fa410642d9e4bf7c
Damian Charles Lowe <damian_c_lowe@yahoo.co.uk>
2022-11-13 12:38:24 +0000

    add Immutable Dictionary

--- src/Immutable.java
+++ src/Immutable.java
@@ -44,6 +44,43 @@ public class Immutable {
 
         bigBoy.removeThing(1);
 //        bigBoy.getStreamOfThings().forEach(System.out::println);
+
+        System.out.println("Try to mutate List used to construct a Container...");
+        final List<Integer> toMutate = new ArrayList<>(Arrays.asList(1,2,3));
+        final Container<Integer> cantMutate = new Container<>(toMutate);
+        System.out.println(cantMutate);
+        toMutate.add(4);
+        System.out.println(cantMutate);
+
+        System.out.println("-----------");
+
+        final Dictionary<Integer, String> dict1 = new Dictionary<>();
+        final Dictionary<Integer, String> dict2 = dict1.addItem(1, "test1");
+        System.out.println(dict2);
+        System.out.println(dict2.removeItem(2));
+        System.out.println(dict2.removeItem(1));
+        System.out.println(dict2.lookupValue(5));
+        System.out.println(dict2.lookupValue(1));
+        final Dictionary<Integer, String> dict3 = dict2
+                .addItem(2, "test2")
+                .addItem(4, "test4")
+                ;
+        dict3.getAllEntriesStream()
+                .map(e -> e.getKey() + " -> " + e.getValue())
+                .forEach(System.out::println);
+
+        System.out.println("-----------");
+        System.out.println("Try to mutate Map used to construct a Dictionary...");
+
+        final Map<Integer, String> mutaMap = new HashMap<>();
+        mutaMap.put(1, "mut1");
+        mutaMap.put(2, "mut2");
+
+        final Dictionary<Integer, String> dict4 = new Dictionary<>(mutaMap);
+        System.out.println(dict4);
+        mutaMap.put(3, "mut3");
+        System.out.println(dict4);
+
     }
 
     public static Optional<Integer> safeParseInt(Object in) {
@@ -95,6 +132,43 @@ public class Immutable {
         @Override
         public String toString() {
             return things.toString();
+        }
+    }
+
+    static class Dictionary<K, V> {
+        private final Map<K, V> dict;
+
+        Dictionary(Map<K, V> dict) {
+            this.dict = new HashMap<>(dict);
+        }
+
+        Dictionary() {
+            this.dict = new HashMap<>();
+        }
+
+        public Dictionary<K, V> addItem(K key, V value) {
+            Map<K, V> updated = new HashMap<>(dict);
+            updated.put(key, value);
+            return new Dictionary<>(updated);
+        }
+
+        public Dictionary<K, V> removeItem(K key) {
+            Map<K, V> updated = new HashMap<>(dict);
+            updated.remove(key);
+            return new Dictionary<>(updated);
+        }
+
+        public Optional<V> lookupValue(K key) {
+            return Optional.ofNullable(dict.get(key));
+        }
+
+        public Stream<Map.Entry<K, V>> getAllEntriesStream() {
+            return dict.entrySet().stream();
+        }
+
+        @Override
+        public String toString() {
+            return dict.toString();
         }
     }
 }
